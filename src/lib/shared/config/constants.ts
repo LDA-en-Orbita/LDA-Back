22784c88f3e584c26d91ae3d0d1c8eefcc4ad15c
Gustavo Farfan <gustavo.farfan@delfosti.com>
2025-10-04 21:29:06 -0500

    fix: resolve file paths

--- src/lib/shared/config/constants.ts
+++ src/lib/shared/config/constants.ts
@@ -1,32 +1,33 @@
+import path from "path";
 import { CodePlanetsEnums } from "src/lib/planets/domain/enums/code-planets.enum";
 import { PlanetConfigType } from "src/lib/planets/domain/type/planet-config.type";
 
 export const APP_CATALOG: Record<CodePlanetsEnums, PlanetConfigType> = {
-  [CodePlanetsEnums.MERCURIO]: {
-    path: "src/lib/planets/infraestructure/database/json/mercury.json",
-  },
-  [CodePlanetsEnums.VENUS]: {
-    path: "src/lib/planets/infraestructure/database/json/venus.json",
-  },
-  [CodePlanetsEnums.TIERRA]: {
-    path: "src/lib/planets/infraestructure/database/json/tierra.json",
-  },
-  [CodePlanetsEnums.MARTE]: {
-    path: "src/lib/planets/infraestructure/database/json/marte.json",
-  },
-  [CodePlanetsEnums.JUPITER]: {
-    path: "src/lib/planets/infraestructure/database/json/jupiter.json",
-  },
-  [CodePlanetsEnums.SATURNO]: {
-    path: "src/lib/planets/infraestructure/database/json/saturno.json",
-  },
-  [CodePlanetsEnums.URANO]: {
-    path: "src/lib/planets/infraestructure/database/json/uranus.json",
-  },
-  [CodePlanetsEnums.NEPTUNO]: {
-    path: "src/lib/planets/infraestructure/database/json/neptuno.json",
-  },
-  [CodePlanetsEnums.PLUTON]: {
-    path: "src/lib/planets/infraestructure/database/json/pluton.json",
-  },
+    [CodePlanetsEnums.MERCURIO]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/mercury.json"),
+    },
+    [CodePlanetsEnums.VENUS]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/venus.json"),
+    },
+    [CodePlanetsEnums.TIERRA]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/tierra.json"),
+    },
+    [CodePlanetsEnums.MARTE]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/marte.json"),
+    },
+    [CodePlanetsEnums.JUPITER]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/jupiter.json"),
+    },
+    [CodePlanetsEnums.SATURNO]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/saturno.json"),
+    },
+    [CodePlanetsEnums.URANO]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/uranus.json"),
+    },
+    [CodePlanetsEnums.NEPTUNO]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/neptuno.json"),
+    },
+    [CodePlanetsEnums.PLUTON]: {
+        path: path.join(__dirname, "../../planets/infraestructure/database/json/pluton.json"),
+    },
 };
